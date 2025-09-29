b260703cc2cf396e9c0a15249f3b23ae785abd2a
Osman Malik <imusmanshakir@gmail.com>
2025-09-29 10:40:15 +0000

    separated curring and closure from HOFs and nested js

--- HOFs_Nested functions/HOF.js
+++ HOFs_Nested functions/HOF.js
@@ -1,5 +1,3 @@
-console.log("****************Topic--> HOFs************************************")
-
 // ****************What is a Higher-Order Function (HOF)?************************
 
 // Definition: A function that takes one or more functions as arguments, returns a function, or both.
@@ -22,34 +20,3 @@ function higherOrderFunction(func){
 
 higherOrderFunction(callbackFunction);
 
-console.log("****************Topic--> Nested Functions************************************");
-// **************************Nested Function**********************************************
-// A nested function (also known as an inner function) is a function that is declared within another function (known as the outer function). 
-// The inner function has access to the variables of its outer function, forming a lexical scope chain.
-
-function outer() {
-    console.log('This is the outer function')
-    function inner() {
-        console.log("This is the inner function.")
-    }
-    inner();
-}
-outer();
-
-
-// Inner Function Accessing Outer Variables
-function outer(a, b) {
-    function inner() {
-        return a + b;
-    }
-    console.log(inner());
-}
-outer(2, 3)//5
-
-
-// How nested functions work in JavaScript?
-// A nested function is a function defined inside another function in JavaScript.
-// The inner function has access to the variables and parameters of the outer function.
-// The inner function can be called only from within the outer function.
-// Nested functions allow for better organization and code reuse.
-// They enable closures, where the inner function can "remember" and access variables from the outer function even after the outer function has finished executing.
