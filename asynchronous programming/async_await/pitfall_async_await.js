885009980e37de9636fd5fa4a7bf5b55a35fb814
imusmanshakir <imusmanshakir@gmail.com>
2025-10-08 13:09:15 +0500

    deleted duplicate files

--- asynchronous programming/async_await/pitfall_async_await.js
+++ asynchronous programming/async_await/pitfall_async_await.js
@@ -1,12 +1,11 @@
 //why first one works and second one doesn't?
 
-
-const url = 'https://dummyjson.com/quotes';
+const url = "https://dummyjson.com/quotes";
 async function fetchData(url) {
   const response = await fetch(url);
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
   const data = await response.json();
   console.log(data);
 }
@@ -20,19 +19,19 @@ fetchData(url);
 //   return data;                   // But function returns IMMEDIATELY
 // }
 
-console.log(fetchData()); // → Promise {<pending>} ⚠️
 
-const url1 = 'https://dummyjson.com/quotes';
+
+const url1 = "https://dummyjson.com/quotes";
 async function fetchData1(url1) {
   const response = await fetch(url1);
-    if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-    }
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
   const data = await response.json();
   return data;
 }
 
-console.log(fetchData1(url));
+console.log(fetchData()); // → Promise {<pending>} ⚠️
 
 // in this example fetchData(url) returns a Promise that resolves to the fetched data, but since we are logging it immediately,
 //  we see the Promise object itself, not the resolved data. To access the actual data, we need to use .then() or await it in another async function.
